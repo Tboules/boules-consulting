71cbba17c093a56b04798f2740a10dcdb987e737
Tony Boules <tboules@gmail.com>
2020-10-17 13:35:21 -0700

    responsive nav bar and cards

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -2,8 +2,27 @@ import { Link } from "gatsby"
 import React from "react"
 import BCImage from "./NewLogo.png"
 import HS from "./header.module.scss"
+import { useState } from "react"
 
 const Header = () => {
+  const [nav, setNav] = useState(null)
+  const [drawerToggle, setDrawerToggle] = useState(false)
+
+  const handleDrawer = () => {
+    if (!drawerToggle) {
+      setDrawerToggle(true)
+      setNav({
+        transform: "translateX(0%)",
+        transition: "transform .5s ease-in-out",
+      })
+    } else {
+      setDrawerToggle(false)
+      setNav({
+        transition: "transform .5s ease-out",
+      })
+    }
+  }
+
   return (
     <div className={HS.shadow}>
       <div className={HS.container}>
@@ -11,7 +30,7 @@ const Header = () => {
           <img alt="logo" src={BCImage} />
           <h2>Boules Consulting</h2>
         </div>
-        <nav className={HS.nav}>
+        <nav style={nav} className={HS.nav}>
           <ul className={HS.navList}>
             <li>
               <Link
@@ -42,6 +61,11 @@ const Header = () => {
             </li>
           </ul>
         </nav>
+        <div onClick={handleDrawer} className={HS.burger}>
+          <div className={HS.line1}></div>
+          <div className={HS.line2}></div>
+          <div className={HS.line3}></div>
+        </div>
       </div>
     </div>
   )
